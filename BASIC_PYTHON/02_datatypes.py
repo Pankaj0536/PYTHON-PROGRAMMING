0342affe8d9b182c2ced207d24d7c3165f6de0b9
Pankaj0536 <pankajrana2754@gmail.com>
2025-09-14 13:57:53 +0530

    New commit

---
+++ BASIC_PYTHON/02_datatypes.py
@@ -0,0 +1,20 @@
+# In this file we will learn various datatpes in python like (int , float , string , etc)
+
+num1 = 34  # integer datatype
+num2 = 45.78  # float datatype
+name = 'Pankaj Rana' # string datatype
+is_student = True  # boolean datatype
+
+# type() this is a function which tells us the datatype of a variable
+
+print(type(num1))
+print(type(num2))
+print(type(name))
+print(type(is_student))
+
+# output look like 
+
+# <class 'int'>
+# <class 'float'>
+# <class 'str'>
+# <class 'bool'>
