2e40586361950c4d91a4b9f4aab2b9108df585b3
Pankaj0536 <pankajrana2754@gmail.com>
2025-09-14 13:40:50 +0530

    first commit in python repo

---
+++ BASIC_PYTHON/01_Print.py
@@ -0,0 +1,11 @@
+# In this file we will learn how a print statements are written in python
+
+
+# printing hello world using python
+print('Hello, learners!')
+
+greet = "Good morning"  #this include datatype we will learn later
+
+print('Hey pankaj',greet)
+
+
