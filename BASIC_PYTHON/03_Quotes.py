9d4bb0d4348db9ba64fe8ac6b9f442ea5b63bedc
Pankaj0536 <pankajrana2754@gmail.com>
2025-09-16 22:29:26 +0530

    New commmit

---
+++ BASIC_PYTHON/03_Quotes.py
@@ -0,0 +1,14 @@
+# This file contain the different types of quotes in python
+
+single_quotes = 'This is a string with single quotes'
+double_quotes = "This is a string with double quotes"
+triple_single_quotes = '''This is a string with triple single quotes'''
+
+# Basically all three types of quotes are used to define string in python
+triple_double_quotes = """This is a string with triple double quotes"""
+# Triple quotes are often used for multi-line strings or docstrings
+
+print(single_quotes)
+print(double_quotes)
+print(triple_single_quotes)
+print(triple_double_quotes)
