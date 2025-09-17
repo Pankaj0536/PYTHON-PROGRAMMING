11dac57c2c5061c2510b3fbb38af7a2b5a7ff59b
Pankaj0536 <pankajrana2754@gmail.com>
2025-09-17 17:10:03 +0530

    new commit

---
+++ BASIC_PYTHON/04_Operations.py
@@ -0,0 +1,21 @@
+# This file contain some arithmetic operations in python
+
+print("\n--- Math Operations ---")
+a = 8
+b = 5
+
+add = a + b
+sub = a - b
+mul = a * b
+div = a / b         # Division (float result)
+floor_div = a // b  # Floor division
+mod = a % b         # Remainder
+power = a ** b      # Exponentiation
+
+print("Addition:", add)
+print("Subtraction:", sub)
+print("Multiplication:", mul)
+print("Division:", div)
+print("Floor Division:", floor_div)
+print("Modulus:", mod)
+print("Exponentiation:", power)
---
+++ BASIC_PYTHON/05_Expressions.py
@@ -0,0 +1,11 @@
+# This file contain various expressions in python
+# Example : a + b / 8 - c * 3 + a**b
+a , b = 2 , 4 # declaration can be done in this way if more variable is initialize at same time
+c , d = 6 , 12
+
+z = a + b / 8 - c * 3 + a**b # expression
+
+# one Way to print the value of variables 
+print(' a = {a}\n b = {b}\n c = {c}\n d = {d}'.format(a=a,b=b,c=c,d=d))
+
+print(' a + b / 8 - c * 3 + a**b = ',z)
