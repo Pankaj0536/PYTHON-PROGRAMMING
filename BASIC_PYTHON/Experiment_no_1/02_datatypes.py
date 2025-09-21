39c4207bd7e55c060a9de41e42b13d21e152425d
Pankaj0536 <pankajrana2754@gmail.com>
2025-09-21 08:45:07 +0530

    modified

--- BASIC_PYTHON/Experiment_no_1/02_datatypes.py
+++ BASIC_PYTHON/Experiment_no_1/02_datatypes.py
@@ -2,19 +2,26 @@
 
 num1 = 34  # integer datatype
 num2 = 45.78  # float datatype
+num3 = 10.5e100 # float with 'e' that indicate power '10'
 name = 'Pankaj Rana' # string datatype
 is_student = True  # boolean datatype
+complex_num = 2 + 3j #complex datatype
 
 # type() this is a function which tells us the datatype of a variable
 
 print(type(num1))
 print(type(num2))
+print(type(num3))
 print(type(name))
 print(type(is_student))
+print(type(complex_num))
+
 
 # output look like 
 
 # <class 'int'>
 # <class 'float'>
+# <class 'float'>
 # <class 'str'>
 # <class 'bool'>
+# <class 'complex'>
