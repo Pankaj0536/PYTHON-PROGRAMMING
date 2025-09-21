0f6d5607b1d3cf5b2a93ae00b436829726bc906d
Lakshya Jain <lakshyajain1508@gmail.com>
2025-09-21 21:37:53 +0530

    Updated that user input
    user can tell that how many number did you want.

--- BASIC_PYTHON/Experiment_no_1/08_Random.py
+++ BASIC_PYTHON/Experiment_no_1/08_Random.py
@@ -1,5 +1,13 @@
 #In this file we we learn how an random number is generated in python 
 import random
 
-for i in range(100) :
-    print(random.randrange(1, 70))
+while True:
+    try:
+        num = int(input("How many random numbers (1-70) would you like to generate? "))
+        break
+    except ValueError:
+        print("Invalid input. Please enter a whole number.")
+
+print(f"\nGenerating {num} random numbers between 1 and 70:")
+for i in range(num):
+    print(random.randint(1, 70))
