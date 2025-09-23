ee9f428de3dda3826788670ac3283ab755298d64
PANKAJ RANA <pankajrana2754@gmail.com>
2025-09-23 09:25:31 +0530

    Update 01_Print.py

--- BASIC_PYTHON/Experiment_no_1/01_Print.py
+++ BASIC_PYTHON/Experiment_no_1/01_Print.py
@@ -1,8 +1,10 @@
 # In this file we will learn how a print statements are written in python
+# syntax : print("Your message")
 
 
-# printing hello world using python
-print('Hello, learners!')
+# Example:
+# printing greeting using python
+print('Hello, learners!') 
 
 greet = "Good morning"  #this include datatype we will learn later
 
