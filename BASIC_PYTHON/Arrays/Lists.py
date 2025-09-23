de1e1cb813187ea17a8a87155e1dcd44469b10d6
Pankaj0536 <pankajrana2754@gmail.com>
2025-09-23 11:27:40 +0530

    new commit

---
+++ BASIC_PYTHON/Arrays/Lists.py
@@ -0,0 +1,21 @@
+# In this file we will learn how file are used
+
+print("---List---")
+
+Vegetables = ["Carrot" , "Potato" , "Cucumber" , "Onion"]
+
+print(" Vegetables   : ",Vegetables)
+
+
+# mutable or changeble
+Vegetables[-1] = "cabbage"
+print("Vegetables are : ",Vegetables)
+
+# Adding Vegetable in exesting list
+Vegetables.append("Chilly") # append() function is used to add anyhthing in existing collection
+print("Vegetables list ", Vegetables)
+
+# Duplicate allowed
+
+stationary = ['Pen','Pencil' , 'Eraser' ,'Pen']
+print("My Stationary are :" ,stationary)
