c8eea44f422f165972d8322198524141e3ae2284
Pankaj0536 <pankajrana2754@gmail.com>
2025-09-25 10:57:02 +0530

    Written statement for immutablity check

--- BASIC_PYTHON/Experiment_no_2(Arrays)/04_Tuples.py
+++ BASIC_PYTHON/Experiment_no_2(Arrays)/04_Tuples.py
@@ -1,22 +1,33 @@
 # In this file we will learn how tupes are used
 
-Students = ("Pankaj" , "Rahul" , "Rohit" ,"Vinay" , "Nisha")
+Students = ("Pankaj" , "Rahul" , "Rohit" ,"Vinay" , "Nisha") #tuple
 
 print("Students are : " , Students)
 
-# immutable or  unchangable:
+# immutable or  unchangable check :
 
-# Students[2] = "Chandan" 
-# print("Students are : " , Students) 
-# TypeError: 'tuple' object does not support item assignment
+try:
+    Students[2] = "Chandan" # This will raise an error
+except TypeError as e:
+    print("Tuples are immutable:", e)
 
-# We cant add new element in exixting tuple using append() function
+# Adding element :
+
+# We cant add new element in existing tuple using append() function
 # Students.append("john") not possible
 
 #Whereas we can add new items using this approach
 Students = Students + ("John",)
 print(Students)
 
+# Dublicate are allowed in tuples :
+Students = Students + ("Pankaj",)
+print("New Tuples is ",Students)
+print("Count of Pankaj is " ,Students.count("Pankaj"))
+
 # output:
 # Students are :  ('Pankaj', 'Rahul', 'Rohit', 'Vinay', 'Nisha')
+# Tuples are immutable: 'tuple' object does not support item assignment
 # ('Pankaj', 'Rahul', 'Rohit', 'Vinay', 'Nisha', 'John')
+# New Tuples is  ('Pankaj', 'Rahul', 'Rohit', 'Vinay', 'Nisha', 'John', 'Pankaj')
+# Count of Pankaj is  2
