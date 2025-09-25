c280d53c34c594cab40b4f6e711a9a877f50b933
Pankaj0536 <pankajrana2754@gmail.com>
2025-09-25 11:44:09 +0530

    new commit

---
+++ BASIC_PYTHON/Experiment_no_2(Arrays)/09_Excercise.py
@@ -0,0 +1,12 @@
+# Create a dictionary where the key is a subject name and the value is a tuple of marks.
+
+Marks_detail = {
+    "Maths" : (12 , 34 ,78),
+    "Hindi" : (78 , 88 , 90),
+    "Science" : (11 , 80 , 78)
+}
+
+print(Marks_detail)
+
+# output:
+# {'Maths': (12, 34, 78), 'Hindi': (78, 88, 90), 'Science': (11, 80, 78)}
