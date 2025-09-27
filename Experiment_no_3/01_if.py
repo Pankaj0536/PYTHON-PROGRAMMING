823db1374522b2681f6d9d515d91caf6e75bc1ba
Pankaj0536 <pankajrana2754@gmail.com>
2025-09-27 14:02:03 +0530

    New commit

---
+++ Experiment_no_3/01_if.py
@@ -0,0 +1,13 @@
+# In this file we will learn to If statements in python
+
+# -- Problem : Check if number is positive  --
+num = int(input("Enter Any Number : "))
+
+if num > 0 :
+    print(f"Number {num} is positive.")
+
+if num < 0 :
+    print(f"Number {num} is Negative.")
+
+if num == 0 :
+    print(f"Number {num} is Zero.")
