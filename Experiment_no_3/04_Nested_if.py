f5fc9a7673e88e85c7eef7f80a98b06481c73bef
Pankaj0536 <pankajrana2754@gmail.com>
2025-09-28 09:52:29 +0530

    new commit

---
+++ Experiment_no_3/04_Nested_if.py
@@ -0,0 +1,10 @@
+# In this file we will compare a age in pointerview of voting criteria and for this we will use nested if statement
+
+age = int(input("Enter Your age : "))
+
+if age>= 18 :
+    if age > 75 :
+        print("Since Your age is greater than 75 , you are not aplicable to give vote! ")
+    else :
+        print("You can give vote.")
+
