2a4397e5898d36040ea99372579c8235fb1d6dc1
Pankaj0536 <pankajrana2754@gmail.com>
2025-10-01 21:18:41 +0530

    Enhanced the explanation

--- Experiment_no_4(Loops)/06_Control_statement_pass.py
+++ Experiment_no_4(Loops)/06_Control_statement_pass.py
@@ -1,7 +1,8 @@
 # Program to use the pass control statement
 
+# The use of pass statement is simple it is used when there is require any statement but we dont want to execute any thing
+
 for i in range(5):
     if i == 2:
-        pass  # Do nothing for i == 2
-    else:
-        print(i)
+        pass # it does nothing 
+    print(i)
