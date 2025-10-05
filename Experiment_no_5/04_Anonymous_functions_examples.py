0608a9094f18fafd4d3b0bfc8e4355750df3d0bb
Pankaj0536 <pankajrana2754@gmail.com>
2025-10-05 22:41:46 +0530

    New commit

---
+++ Experiment_no_5/04_Anonymous_functions_examples.py
@@ -0,0 +1,13 @@
+# In this file we will run some Anonymous functions examples
+
+add_num = lambda a, b: a + b
+multiply_num = lambda a , b : a * b
+divide_num = lambda a , b : a/b 
+power_calculate = lambda a , b : a**b
+
+print(f" 23 + 45 = {add_num(23 , 45)}")
+print(f" 7 * 8 = {multiply_num(7 , 8)} ")
+print(f" 80 / 8 = {divide_num(7 , 8)} ")
+print(f" 9 ^ 3 = {power_calculate(9 , 3)} ")
+
+
