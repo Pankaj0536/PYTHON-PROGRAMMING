cf3b4cfa18a835cae276db23115479d6045c4e5a
Pankaj0536 <pankajrana2754@gmail.com>
2025-10-07 20:45:18 +0530

    New commit

---
+++ Assignments/02_.py
@@ -0,0 +1,9 @@
+# Write Python code to print numbers from 2 to 10 with a step size of 2 using the range() function.
+
+# Syntax for range() :
+#   range(start , end , step )
+#   here , start is included , wherares end is not included 
+#   step is the jump like i++ (for sigle step , step = 1 or dont write )
+
+for i in range(2 , 11 , 2) : 
+    print(i , end = " ")
---
+++ Assignments/03_.py
@@ -0,0 +1,7 @@
+# Create a Python set containing the names of three programming languages and display it.
+
+languages = {"C", "C++", "Python"}
+
+print("These are some basic programming languages:")
+for lang in languages:
+    print("-", lang)
