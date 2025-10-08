e714141800562e947818dc5569c3ffb0dc1c06be
Pankaj0536 <pankajrana2754@gmail.com>
2025-10-08 23:52:29 +0530

    New commit

---
+++ Assignments/05_.py
@@ -0,0 +1,14 @@
+# Convert the following list into a dictionary where elements are keys and their length is the value: ['apple', 'banana', 'kiwi']
+
+# Given List
+fruit_list = ['apple', 'banana', 'kiwi']
+
+# Dictionary conversion 
+fruit_dict = {
+    'apple' : len('apple'),
+    'banana' : len('banana'),
+    'kiwi' : len('kiwi')
+}
+
+for fruit in fruit_dict :
+    print(fruit)
