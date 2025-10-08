ea6f9ba2eaa5f9d7a26d38cb2d5051a751527f05
Pankaj0536 <pankajrana2754@gmail.com>
2025-10-09 00:53:02 +0530

    New commit

---
+++ Assignments/08_.py
@@ -0,0 +1,16 @@
+# Define a function greet(name) that prints "Hello, <name>". Call it with your name.
+
+# Function defination
+def greet(name):
+    print("Hello",name )
+
+name = input("Enter your name : ")
+
+# Function calling
+greet(name)
+
+'''
+output : 
+Enter your name : Pankaj
+Hello Pankaj
+'''
