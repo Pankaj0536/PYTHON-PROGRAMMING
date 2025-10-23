75f0ed0938e4caefafe3bb0aac0e7c8f1766fc19
Pankaj0536 <pankajrana2754@gmail.com>
2025-10-24 00:15:03 +0530

    New commit

---
+++ Experiment_no_1/09_Parameters.py
@@ -0,0 +1,20 @@
+'''
+ In this file we will learns and use some parameters  in print statements
+ print(object(s), sep, end, file, flush)
+    sep: It is an optional parameter used to define the separation among different objects to be printed.
+    end is also a pramentr , definethat how the print statement going to end 
+ '''
+
+# example :
+
+# Use of sep parameter 
+name = "pankaj"
+age = 19
+
+print(name , age , 23, sep = ' -> ')
+
+# use of end parameter
+print ("Hey this is pankaj here", end = " ")
+print("and pankaj age is 19")
+
+#  there are many parameters in python like file , flush
