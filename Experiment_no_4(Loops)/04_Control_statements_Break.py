83cba34dedea551d632b3802bb42bf59936ba582
Pankaj0536 <pankajrana2754@gmail.com>
2025-11-24 23:45:23 +0530

    Corrected logic issue

--- Experiment_no_4(Loops)/04_Control_statements_Break.py
+++ Experiment_no_4(Loops)/04_Control_statements_Break.py
@@ -2,9 +2,21 @@
 
 while 1 :
     num = int(input("Enter any value (Enter 0 to stop) : "))
+
+    if num == 0:
+        print("Stopping the Execution....")
+        break
     if num % 2 == 0 :
         print(f"Number {num} is even")
     else :
         print(f"Number {num} is odd")
-    if num == 0:
-        break
+
+'''
+Output : 
+Enter any value (Enter 0 to stop) : 2
+Number 2 is even
+Enter any value (Enter 0 to stop) : 1
+Number 1 is odd
+Enter any value (Enter 0 to stop) : 0
+Stopping the Execution....
+'''
